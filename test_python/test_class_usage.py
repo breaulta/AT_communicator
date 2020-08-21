658f6e7431133cc755ceef83750636fbe9d41642
Aaron <aaronbreault@gmail.com>
2020-08-20 17:22:40 -0700

    Added some notes for next time development

--- test_python/test_class_usage.py
+++ test_python/test_class_usage.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python
 
+
+#note for next time:
+#	add turn sim card on functionality to class
+#	and verifiy that it's on
+#	add periodic new sms functionality to class
+#	could be done by spawning separate process
+	#separate script that reboots our SIM script when pi power cycles
+
+
 #Don't create a pesky .pyc file.
 import sys
 sys.dont_write_bytecode = True
