9a528659a43b237d2fd0294a0a9d63ed09e34198
Aaron <aaronbreaul@gmail.com>
2020-09-03 19:47:40 -0700

    Adding usb clobber test script

---
+++ test_python/usb_clobber_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#Don't create a pesky .pyc file.
+import os
+import sys
+import time
+sys.dont_write_bytecode = True
+
+sim_path = '/dev/cdc-wdm0'
+print 'turn online'
+os.system("qmicli -d " + sim_path + " --dms-set-operating-mode='online'")
+time.sleep(10)
+print 'set to reset'
+os.system("qmicli -d " + sim_path + " --dms-set-operating-mode='reset'")
+time.sleep(10)
+print 'turn back online'
+os.system("qmicli -d " + sim_path + " --dms-set-operating-mode='online'")
