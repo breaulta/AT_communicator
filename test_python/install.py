898de045befc376d853288b6be5e614732b7a725
Aaron <aaronbreaul@gmail.com>
2020-09-11 17:01:45 -0700

    moar install

--- test_python/install.py
+++ test_python/install.py
@@ -10,11 +10,17 @@ main_script = "startup_test.py"
 rc_local_file = open("/etc/rc.local", "r")
 rc_local_content = rc_local_file.read()
 rc_local_list = rc_local_content.split("\n")
+rc_local_file.close()
+rc_local_fd = open("/etc/rc.local", "w")
 for line in rc_local_list:
 	if line == 'exit(0)':
-		print "cd " + run_directory
-		print "sudo python " + run_directory + " " + main_script
-	print "~~~" + line + "~~~"
+		#print "cd " + run_directory
+		rc_local_fd.write("cd " + run_directory)
+		#print "sudo python " + run_directory + " " + main_script
+		rc_local_fd.write("sudo python " + run_directory + " " + main_script)
+	#print "~~~" + line + "~~~"
+	rc_local_fd.write(line)
+
 
 #insert cd into our main directory
 #insert 'python /path/mainfile.py &'
