5e1e61b652084f7edd3b3c0df0b25b243f641069
Aaron <aaronbreaul@gmail.com>
2020-09-14 18:01:11 -0700

    fixed whitespace so python is happy

--- test_python/little_free_locker.py
+++ test_python/little_free_locker.py
@@ -7,38 +7,38 @@ import os
 
 class Locker:
 	def __init__(self, **kwargs):
-        if 'name' in kwargs:
-            self.name = kwargs['name']
-        else:
+		if 'name' in kwargs:
+			self.name = kwargs['name']
+		else:
 			raise Exception ("The locker needs a name.")
-        if 'combo' in kwargs:
-            self.combo = kwargs['combo']
-        else:
+		if 'combo' in kwargs:
+			self.combo = kwargs['combo']
+		else:
 			raise Exception ("The locker needs a combo.")
-        if 'address' in kwargs:
-            self.address = kwargs['address']
-        else:
+		if 'address' in kwargs:
+			self.address = kwargs['address']
+		else:
 			raise Exception ("The locker needs an address.")
-        if 'host_number' in kwargs:
-            self.host_number = kwargs['host_number']
-        else:
+		if 'host_number' in kwargs:
+			self.host_number = kwargs['host_number']
+		else:
 			raise Exception ("The locker needs a contact number for the host.")
-        if 'current_borrower_number' in kwargs:
-            self.current_borrower_number = kwargs['current_borrower_number']
-        if 'checkout_time_length' in kwargs:
-            self.checkout_time_length = kwargs['checkout_time_length']
-        else:
+		if 'current_borrower_number' in kwargs:
+			self.current_borrower_number = kwargs['current_borrower_number']
+		if 'checkout_time_length' in kwargs:
+			self.checkout_time_length = kwargs['checkout_time_length']
+		else:
 			raise Exception ("The locker needs to know how long to be active per user session.")
-        if 'start_date' in kwargs:
-            self.start_date = kwargs['start_date']
-        if 'total_renewals_possible' in kwargs:
-            self.total_renewals_possible = kwargs['total_renewals_possible']
+		if 'start_date' in kwargs:
+			self.start_date = kwargs['start_date']
+		if 'total_renewals_possible' in kwargs:
+			self.total_renewals_possible = kwargs['total_renewals_possible']
 		else:
 			self.total_renewals_possible = 0
-        if 'renewals_used' in kwargs:
-            self.renewals_used = kwargs['renewals_used']
+		if 'renewals_used' in kwargs:
+			self.renewals_used = kwargs['renewals_used']
 		else:
-			self.renewals_used = 0
+		self.renewals_used = 0
 
 
 
