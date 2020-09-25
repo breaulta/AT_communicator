eeca66a0ba17162a3f82182487e7f8ef5a65d228
Aaron <aaronbreault@gmail.com>
2020-09-25 15:35:43 -0700

    fixed read in script

--- test_python/test_locker_serialization.py
+++ test_python/test_locker_serialization.py
@@ -11,25 +11,9 @@ locker_database_file = "locker_database.json"
 locker = Locker(name='testlock', combo='32.51.0', address='1000 main street', host_number='5039895540', current_borrower_number='3335554444', checkout_time_length='14', start_date='9.5.2020', renewals_possible='1', renewals_used='1')
 locker2 = Locker(name='testlock2', combo='32.51.0', address='1000 main street', host_number='5039895540', current_borrower_number='3335554444', checkout_time_length='14', start_date='9.5.2020', renewals_possible='1', renewals_used='1')
 #serialize them
-locker.save_locker_obj_to_json_file(locker_database_file)
 #reload them
 #lkr = Locker
-lkr = locker.json_file_to_locker_obj(locker_database_file)
 
-"""
-lockers_obj = Lockers()
-lockers_obj.add_locker(lkr)
-lockers_obj.add_locker(locker2)
-
-
-lockers_obj.print_lockers()
-lockers_obj.save_lockers_to_json_file("lockers_output.dat")
-
-new_lockers_obj = Lockers()
-new_lockers_obj.json_file_to_lockers_obj("lockers_output.dat")
-
-new_lockers_obj.print_lockers()
-"""
 
 
 filename = "template.txt"
@@ -37,7 +21,7 @@ infile = open(filename, 'r')
 for line in infile:
 	print "line: " + line
 
-outfile.close()
+infile.close()
 
 
 
