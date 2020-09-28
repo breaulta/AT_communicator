fcb690ee323c7614329361373758cc3c988aa06f
Aaron <aaronbreaul@gmail.com>
2020-09-28 16:49:35 -0700

    modifying test file

--- test_python/test_locker_serialization.py
+++ test_python/test_locker_serialization.py
@@ -6,20 +6,12 @@ sys.dont_write_bytecode = True
 from little_free_locker import Locker
 from little_free_locker import Lockers
 
-locker_database_file = "locker_database.json"
-
-#create a couple locker objects
-locker = Locker(name='testlock', combo='32.51.0', address='1000 main street', host_number='5039895540', current_borrower_number='3335554444', checkout_time_length='14', start_date='9.5.2020', renewals_possible='1', renewals_used='1')
-locker2 = Locker(name='testlock2', combo='32.51.0', address='1000 main street', host_number='5039895540', current_borrower_number='3335554444', checkout_time_length='14', start_date='9.5.2020', renewals_possible='1', renewals_used='1')
-#serialize them
-#reload them
-#lkr = Locker
 
 lockerobj_from_user_file = Lockers()
 lockerobj_from_user_file.load_lockers_from_user_input_txt_file("template.txt")
 
 lockerobj_from_user_file.print_lockers()
-lockerobj_from_user_file.save_lockers_to_json_file('saving_lockers_to_json.txt')
+lockerobj_from_user_file.save_lockers_to_json_file()
 
 
 
