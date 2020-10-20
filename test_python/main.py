466c753b77ff965042a61cdf77d242944e9feab8
Aaron <aaronbreaul@gmail.com>
2020-10-20 16:05:36 -0700

    Added notes for next sesh after 18 day break.

--- test_python/main.py
+++ test_python/main.py
@@ -24,28 +24,47 @@ tx = Transmitter(port = '/dev/ttyUSB2')
 
 #infinite loop
 while(1):
-	
+
+	#Ensure sim/hat is set up and able to send/receive texts.
 	tx.ensure_sim_card_connected_to_network("/dev/cdc-wdm0")
 	#Read in any potential new texts (incoming sms).
 	print "We're running get all texts now!"
+	#Query sim/hat for any new texts it has received.
 	new_sms_array = tx.get_all_texts()
 
-	#Check if we have new texts...
+	#If there are texts in the new_sms_array, we know we have new texts to process.
 	if len(new_sms_array) > 0:
 		print "We have new messages!"
+		#Save any new texts from the sim card to the pi's hard drive.
 		tx.append_texts_to_db_file(new_sms_array, sms_database_filename)
+		#Now that we've saved them from the hard drive, we can delete from the sim card.
 		tx.delete_texts_from_sim_card(new_sms_array)
+
+		#~~~~~~~~~~~~~~~~~~~~
+		#		locker.respond_to_new_texts(tx, new_sms_array)
+		# not sure if we wanted to make this method or were going to do it another way.
+		#~~~~~~~~~~~~~~~~~~
+
 		#Little free locker response to new texts
-		#locker.respond_to_new_texts(tx, new_sms_array)
 		#Incoming sms should be only the name of the locker they're trying to checkout.
 		for sms in new_sms_array:
-			print "received sms message: ~" + sms.message + "~"
+			print "received sms message: ~" + sms.message + "~" #debugging
+			#Cycle through lockers to see if the incomming sms matches a locker name.
 			for locker in main_lockers.lockers:
-				#maybe regex here to clean up incoming sms.
+				#In order for the (\b) to work we needed the r.
+				#We are doing the best we can to extract the locker name from the incomming sms message.
 				m = re.search(r'^\s*\b(.+)\b\s*$', sms.message)
 				if m:
-					#If we matched the 'name' case-insensitively at the beginning of locker parameters.
+					#Match case-insensitively the name of the locker to the text message.
 					if m.groups()[0].lower() == locker.name.lower():
+#Notes for next time:
+#	Calculate due_date in locker method for is_locker_checked_out method
+#	Create is_locker_checked_out method
+#	Create checkout_locker method
+#	
+						
+
+
 						#Check if locker is currently checked out.
 
 						#Calculate date/timing data.
