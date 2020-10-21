8ec592a28e84de7fc1469dd565d61d547bf90074
Aaron <aaronbreaul@gmail.com>
2020-10-21 16:08:54 -0700

    testing

--- test_python/little_free_locker.py
+++ test_python/little_free_locker.py
@@ -195,7 +195,7 @@ class Locker:
 		self.due_date = self.serialize_date(due_date)
 
 	def is_locker_checked_out(self):
-		if self.due_date:
+		if has_attr(self, due_date):
 			#Is checked out
 			return 1
 		else:
