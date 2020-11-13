8cd978ed110eaed260b30fbc4ce2927b0be7f32a
Aaron <aaronbreaul@gmail.com>
2020-11-12 17:13:18 -0800

    converts string to gsm7 for pdu transfer

---
+++ hex_conversion_for_pdu.py
@@ -0,0 +1,40 @@
+import sys
+import unicodedata
+import binascii
+
+message = 'hello world'
+hex_message = ''
+for c in message:
+	hex_message += hex(ord(c))[2:]
+
+print hex_message
+
+print binascii.b2a_hex(message)
+
+
+exit(0)
+
+uni = u'\u054c'
+print uni
+print repr(uni)
+
+exit(0)
+message = 'hello world'
+print('orig: ' + message)
+bytes(message)
+unicoded = unicode(message, 'utf-8')
+
+decoded = unicoded.decode('utf-8')
+print decoded
+print repr(decoded)
+
+
+
+exit(0)
+sys.stdout.write(repr(u.encode('utf-8')))
+encoded = decoded.encode('ascii')
+sys.stdout.write(repr('encoded: ' + encoded))
+#sys.stdout.buffer.write(encoded)
+#decoded = encoded.decode('utf-8')
+#sys.stdout.buffer.write(decoded)
+
