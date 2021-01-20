52188d2212cceb505a27f41d69776f10762818ee
Aaron <aaronbreaul@gmail.com>
2021-01-20 14:04:37 -0800

    moreping

--- test_python/module.py
+++ test_python/module.py
@@ -326,7 +326,7 @@ class Transmitter:
 			octets = self.encode_gsm_octets(SM_part)		#byte array
 			user_data_length = len(octets) + 6		#number of septets + 6 for the number of octets in the UDH (this seems wrong)
 			print "udl = " + str(user_data_length)
-			user_data = packSeptets(octets)	#byte array
+			user_data = self.packSeptets(octets)	#byte array
 			pdu.append(service_center_address)
 			pdu.append(message_type_indicator)
 			pdu.append(self.message_ref)
