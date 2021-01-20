52df7d33eb359bd5d61f7e3903418fe028f62508
Aaron <aaronbreaul@gmail.com>
2021-01-20 15:35:11 -0800

    ping

--- test_python/module.py
+++ test_python/module.py
@@ -325,7 +325,7 @@ class Transmitter:
 		for SM_part in message_list_pdu:
 			pdu = bytearray()
 			octets = self.encode_gsm_octets(SM_part)		#byte array
-			user_data_length = len(octets) + 6		#number of septets + 6 for the number of octets in the UDH (this seems wrong)
+			user_data_length = len(octets) + 7		#number of septets + 7 for the number of octets in the UDH (this seems wrong)
 			print "udl = " + str(user_data_length)
 			user_data = self.packSeptets(octets)	#byte array
 			pdu.append(service_center_address)
