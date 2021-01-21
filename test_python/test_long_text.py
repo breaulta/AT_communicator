d2e05c6afbdd4e56c20780e0fadc972d9c066bf9
Aaron <aaronbreaul@gmail.com>
2021-01-20 16:37:36 -0800

    WORKED! Testing another text...

--- test_python/test_long_text.py
+++ test_python/test_long_text.py
@@ -10,4 +10,9 @@ from little_free_locker import Lockers
 
 
 tx = Transmitter()
-tx.send_text('5039895540', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+tx.send_text('5039895540', 'Major changes/decisions I’ve made that I would like to go over with Len
+Hard coding UDH/Fill bits in the PDU because we are only going to use it for sending Concatenated SMS
+Piecing together elements of a couple different scripts in order to implement PDU CSM.
+I found a whole gsm modem implementation that we could have potentially used at the beginning, but ended up taking pieces of the lower level code.
+8-bit is not a possibility as it doesn’t have an alphabet attached to it.
+Aaron dont ever forget how much of a beast your are XD')
