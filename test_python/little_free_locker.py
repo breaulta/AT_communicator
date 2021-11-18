d9e1a3db6d6f32fbbbe253a39c07fa99e87d805d
Aaron <aaronbreault@gmail.com>
2021-11-17 17:36:41 -0700

    renew message spawner looks to be working

--- test_python/little_free_locker.py
+++ test_python/little_free_locker.py
@@ -189,10 +189,10 @@ class Locker:
 			self.renewals_used = kwargs['renewals_used']
 		else:
 			self.renewals_used = "0"
-		if '24hourflag' in kwargs:
-			self.24hourflag = kwargs['24hourflag']
-		if '48hourflag' in kwargs:
-			self.48hourflag = kwargs['48hourflag']
+		if 'onedayflag' in kwargs:
+			self.onedayflag= kwargs['onedayflag']
+		if 'twodayflag' in kwargs:
+			self.twodayflag= kwargs['twodayflag']
 		
 	
 	#Stringify a datetime object for storage in a json file.
@@ -224,6 +224,9 @@ class Locker:
 			#record tenant number
 			self.tenant_number = tenant_number
 			self.renewals_used = '0'
+			#set renewal flags
+			self.onedayflag = 1
+			self.twodayflag = 1
 
 	def is_locker_checked_out(self):
 		if hasattr(self, "due_date"):
--- test_python/locker_test.py
+++ test_python/locker_test.py
@@ -10,9 +10,11 @@ sys.dont_write_bytecode = True
 from little_free_locker import Locker
 from little_free_locker import Lockers
 
-locker_bank = Lockers() #init
-locker_bank.load_lockers_from_user_input_txt_file("template.txt") #adds lockers from user input file
+main_lockers = Lockers() #init
+main_lockers.load_lockers_from_user_input_txt_file("template.txt") #adds lockers from user input file
 
+
+"""
 lst = locker_bank.get_locker_list()
 for locker in lst:
 	print locker
@@ -34,28 +36,35 @@ if hours <= 48:
 	print "due"
 else:
 	print "not due"
+"""
 
+obj = main_lockers.get_locker_obj_given_locker_name('Nala')
+obj.checkout_locker("5039895540")
+obj.due_date = '11/19/2021'
 
-
-exit(0)
 #Spawn renewal messages
 while 1:
 	now = datetime.now() #keep calculating
 	bank = main_lockers.get_locker_list() #from main.pl initialization
 	for lockername in bank:
 		locker_obj = main_lockers.get_locker_obj_given_locker_name( lockername )
-		duedate = locker_obj.deserialize_date( locker_obj.due_date )
-		diff = duedate - now
-		seconds = diff.total_seconds()
-		hours = seconds / 3600
-		print hours #debug
-		#renew hours left check
-		if hours <= 24 and locker_obj.24hourflag = 1:
-			print "24 hour message"
-		elif hours <= 48 and locker_obj.48hourflag = 1:
-			print "48 hour message"
-		else:
-			print "not due"
+		if hasattr(locker_obj, 'due_date'):
+			duedate = locker_obj.deserialize_date( locker_obj.due_date )
+			diff = duedate - now
+			seconds = diff.total_seconds()
+			hours = seconds / 3600
+			print hours #debug
+			#renew hours left check
+			if hours <= 24 and locker_obj.onedayflag == 1:
+				locker_obj.onedayflag = 0
+				print "24 hour message"
+				continue
+			elif hours <= 48 and locker_obj.twodayflag == 1:
+				locker_obj.twodayflag = 0
+				print "48 hour message"
+			else:
+				print "not due"
+	time.sleep(2)
 
 
 #Need timer to track checkout period for each locker.
