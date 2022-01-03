eb5c4bf7220eafeea0a3f72fdd35e0e4110e8914
Aaron <aaronbreault@gmail.com>
2022-01-03 14:39:32 -0700

    test logger now logs stderr to file

--- test_python/log_testing.py
+++ test_python/log_testing.py
@@ -16,15 +16,39 @@ logger.setLevel(logging.DEBUG)	# Set default level?
 fh = logging.FileHandler('locker.log')
 fh.setLevel(logging.DEBUG)
 # create console handler with a higher log level
-ch = logging.StreamHandler()
-ch.setLevel(logging.ERROR)
+#ch = logging.StreamHandler()
+#ch.setLevel(logging.ERROR)
 # create formatter and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
-ch.setFormatter(formatter)
+#ch.setFormatter(formatter)
 # add the handlers to the logger
 logger.addHandler(fh)
-logger.addHandler(ch)
+#logger.addHandler(ch)
+
+class StreamToLogger(object):
+   """
+   Fake file-like stream object that redirects writes to a logger instance.
+   """
+   def __init__(self, logger, log_level=logging.INFO):
+      self.logger = logger
+      self.log_level = log_level
+      self.linebuf = ''
+
+   def write(self, buf):
+      for line in buf.rstrip().splitlines():
+         self.logger.log(self.log_level, line.rstrip())
+
+stdout_logger = logging.getLogger('test_logger_app')
+sl = StreamToLogger(stdout_logger, logging.INFO)
+sys.stdout = sl
+
+stderr_logger = logging.getLogger('test_logger_app')
+sl = StreamToLogger(stderr_logger, logging.ERROR)
+sys.stderr = sl
+
+#print "Test to standard out"
+#raise Exception('Test to standard error')
 
 from little_free_locker import Locker
 from little_free_locker import Lockers
@@ -34,7 +58,7 @@ locker_bank = Lockers()
 logger.info('load from template')
 locker_bank.load_lockers_from_user_input_txt_file("template.txt")
 
-locker_bank.checkout_locker('5039895540', 'Nala')
+locker_bank.checkout_locker('5039895540', 'ala')
 
 locker_bank.freeup_locker('Nala')
 #nala = locker_bank.get_locker_object_given_locker_name('Nala')
