efc092991d8f3e58e0bcc5a5261558a3b87254c9
Aaron <aaronbreault@gmail.com>
2022-01-04 11:29:15 -0700

    new way of processing incoming sms

---
+++ test_python/handle_and_convert_sms.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import time
+from datetime import datetime, timedelta
+import serial
+import re
+import json
+import os
+import sys
+#Don't write a pesky .pyc file.
+sys.dont_write_bytecode = True
+
+# test input sms object
+
+# write object datas to file
+
+# main loop periodically scans a folder for a new file with unique name
+
+# when one is found, scan in the datas and delete file
+
+# act on the datas
