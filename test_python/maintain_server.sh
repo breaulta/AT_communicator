d088c3a31aa8659df64fa950c308014ed9f055c4
Aaron <aaronbreault@gmail.com>
2022-01-13 15:00:30 -0700

    main.py starts up on reboot. saving rc.local for reference

--- test_python/maintain_server.sh
+++ test_python/maintain_server.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
+# Respawns main.py unless a graceful exit code is received.
 
-while true
-do
-sudo python main.py
-sleep 1
+until sudo python main.py; do
+	echo "Server main.py crashed with exit code $?. Respawning..." >&2
+	sleep 1
 done
